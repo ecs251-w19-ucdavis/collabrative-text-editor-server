fa48ee0101f9a4e8531c34a51313505aace43489
yichlin <yichlin@indiana.edu>
2019-02-28 02:42:11 -0800

    c

--- collaborative_text_editor_python/app.py
+++ collaborative_text_editor_python/app.py
@@ -7,10 +7,21 @@ import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
 # db object
 db = SQLAlchemy(app)
 
-from model import *
+
+class User(db.Model):
+    userID = db.Column(db.String, primary_key=True)
+    docName = db.Column(db.String, primary_key=True, nullable=True)
+    docID = db.Column(db.String, nullable=True)
+
+
+db.create_all()
+
+db.session.commit()
+
 
 @app.route('/')
 def index():
@@ -19,75 +30,78 @@ def index():
 
 @socketio.on('join')
 def joined(message, methods=['GET', 'POST']):
-    join_room(message["room"])
-    socketio.emit('join', {'msg': message["user"] + ' has entered the room.'}, room=message["room"])
+    socketio.emit('join', {'msg': message["user"] + ' has entered the room.'}, namespace="/" + message["docID"])
 
 
 @socketio.on('MSG')
 def recieve_msg(json, methods=['GET', 'POST']):
-    socketio.emit('MSG', json, room=json["room"])
+    socketio.emit('MSG', json, namespace="/" + json["docID"])
 
 
 @socketio.on('DOC')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    socketio.emit('DOC', json, room=json["room"])
+def recieve_doc(json, methods=['GET', 'POST']):
+    socketio.emit('DOC', json, namespace="/" + json["docID"])
 
-@socketio.on('create_file')
-def create_file(json, methods = ['GET', 'POST']):
+
+@socketio.on('create_new_doc')
+def create_file(json, methods=['GET', 'POST']):
     userID = json["userID"]
-    docID = uuid.uuid4()
+    docID = str(uuid.uuid4())
     docName = json["docName"]
 
-    # check if the file already exists
-    if User.query.filter_by(userID=userID, docName=docName):
-        #return error message to the client
-        pass
-
-    # file does not exist
     user = User(userID=userID, docName=docName, docID=docID)
-    with open(docID+".txt","w") as file:
+    with open(docID + ".txt", "w") as file:
+        print()
         file.write('')
     db.session.add(user)
-    db.commit()
+    db.session.commit()
 
+    socketio.emit('response_create_doc', {"docID":docID},room=userID)
     # client should open the file now
 
-@socketio.on('get_files')
-def get_files(json, methods = ['GET', 'POST']):
+
+@socketio.on('request_doc_list')
+def get_files(json, methods=['GET', 'POST']):
     userID = json["userID"]
+    join_room(userID)
     files = User.query.filter_by(userID=userID)
     response = []
     for file in files:
-        response.append(file.docName)
-    response = json.dumps(response)
-    # return response to the client
+        response.append({"docName": file.docName, "docID": file.docID})
+    socketio.emit('response_doc_list', response,room=userID)
 
-@socketio.on('read_file')
-def read_file(json, methods = ['GET', 'POST']):
+
+@socketio.on('request_doc_content')
+def read_file(json, methods=['GET', 'POST']):
+    docID = json["docID"]
+    userID = json["userID"]
+    with open(docID + ".txt", "r") as file:
+        response = file.read()
+        socketio.emit('response_doc_content', {"docID":docID, "content":response},room=userID)
+
+
+@socketio.on('share')
+def join_file(json, methods=['GET', 'POST']):
     userID = json["userID"]
     docName = json["docName"]
-    user = User.query.filter_by(userID=userID, docName=docName).first()
-    docID = user.docID
-    with open(docID+".txt","r") as file:
-        response = file.read
-    response = json.dumps(response)
-    # return response to the client
-
-@socketio.on('join_file')
-def join_file(json, methods = ['GET', 'POST']):
-    userID = json["userID"]
-    docName = json["docName"]
+    docID = json["docID"]
 
     # check if the user is already collaborating this file
-    if User.query.filter_by(userID=userID, docName=docName):
-        #return error message to the client
-        pass
+    if User.query.filter_by(userID=userID, docName=docName, docID=docID).all():
+        return
 
-    docID = User.query.filter_by(userID=userID, docName=docName).first().docID
     user = User(userID=userID, docName=docName, docID=docID)
     db.session.add(user)
-    db.commit()
-    # return response to the client
+    db.session.commit()
+
+
+@socketio.on('save')
+def save_file(json, methods=['GET', 'POST']):
+    docID = json["docID"]
+
+    with open(docID + ".txt", "w") as file:
+        file.write(json["doc"])
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0')
--- collaborative_text_editor_python/static/script.js
+++ collaborative_text_editor_python/static/script.js
@@ -10,55 +10,109 @@ var userId = "";
 while (userId == "") {
     userId = prompt("Please enter your UserId", "");
 }
-/*
-* TODO:
-* 1. fetch user's file list from server
-* 2. ask user which file to open
-* */
 
-// Here we assume userId 1 and 2 are editing file1
-// and userId 3 is editing file2
-var fileId;
-if (userId == "1" || userId == "2") {
-    fileId = "file1";
-} else {
-    fileId = "file2";
-}
-
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-
-socket.on('connect', function () {
-    socket.emit('join', {"user": userId, 'room': fileId});
+var server_socket = io.connect('http://' + document.domain + ':' + location.port);
+var doc_socket = io("/");
+var doc_ID;
+var doc_name;
+server_socket.on('connect', function () {
+    server_socket.emit('request_doc_list', {"userID": userId});
 });
 
-socket.on('join', function (data) {
-    var msgBoard = document.getElementById("messageBoard");
-    msgBoard.innerHTML += data['msg'] + "<br>";
-});
+server_socket.on('response_doc_list', function (doc_list) {
+    var choice = "";
+    var instruction = "";
 
-socket.on('MSG', function (data) {
-    var msgBoard = document.getElementById("messageBoard");
-    msgBoard.innerHTML += data["user"] + ": " + data['text'] + "<br>";
-});
+    for (var i = 0; i < doc_list.length; i++) {
+        instruction += i + ". " + doc_list[i]["docName"];
+    }
+    instruction += doc_list.length + ". " + "Create new document";
+    while (choice == "" || parseInt(choice) > doc_list.length) {
+        choice = prompt(instruction, "");
+    }
 
-socket.on('DOC', function (data) {
-    console.log("receive");
-    if (data["user"] != userId) {
-        document.getElementById("editor").value = data['doc'];
+    if (parseInt(choice) == doc_list.length) {
+        var new_doc_name = "";
+        while (new_doc_name == "") {
+            new_doc_name = prompt("Please input your new document name", "");
+            for (var i = 0; i < doc_list.length; i++) {
+                if (new_doc_name == doc_list[i]["docName"]) {
+                    alert("Duplicated document name!");
+                    break;
+                }
+            }
+        }
+        doc_name = new_doc_name;
+        server_socket.emit("create_new_doc", {"userID": userId, "docName": new_doc_name});
+    } else {
+        doc_ID = doc_list[parseInt(choice)]["docID"];
+        doc_name = doc_list[parseInt(choice)]["docName"]
+        server_socket.emit("request_doc_content", {"docName":doc_name, "docID":doc_ID, "userID":userId});
     }
 });
 
+server_socket.on('response_create_doc', function (data) {
+    doc_ID = data["docID"];
+    doc_socket = io("/" + data["docID"]);
+    defineServerCommunication(doc_socket)
+});
+
+server_socket.on("response_doc_content", function (doc_data) {
+    document.getElementById("editor").value = doc_data["content"];
+    doc_socket = io("/" + doc_data["docID"]);
+    defineServerCommunication(doc_socket);
+});
+
+function defineServerCommunication(doc_socket) {
+    doc_socket.on('connect', function () {
+        doc_socket.emit('join', {"user": userId, "docID": doc_ID});
+    });
+
+    doc_socket.on('join', function (data) {
+        var msgBoard = document.getElementById("messageBoard");
+        msgBoard.innerHTML += data['msg'] + "<br>";
+    });
+
+    doc_socket.on('MSG', function (data) {
+        var msgBoard = document.getElementById("messageBoard");
+        msgBoard.innerHTML += data["user"] + ": " + data['text'] + "<br>";
+    });
+
+    doc_socket.on('DOC', function (data) {
+        if (data["user"] != userId) {
+            document.getElementById("editor").value = data['doc'];
+        }
+    });
+}
 
 function keyUpMSG(event) {
     var msg = document.getElementById("input").value;
     var x = event.which || event.keyCode;
     if (x == 13) {
-        socket.emit('MSG', {"user": userId, "text": msg, 'room': fileId});
+        doc_socket.emit('MSG', {
+            "user": userId,
+            "text": msg,
+            "docID": doc_ID
+        }, namespace = "/" + doc_ID);
         document.getElementById("input").value = "";
     }
 }
 
 function keyUpDOC(event) {
     var doc = document.getElementById("editor").value;
-    socket.emit('DOC', {"doc": doc, "user": userId, 'room': fileId});
+    doc_socket.emit('DOC', {"doc": doc, "user": userId, "docID": doc_ID});
+}
+
+function share() {
+    var target = "";
+    while (target == "") {
+        target = prompt("Please input who you want to share the document with?")
+    }
+    server_socket.emit("share", {"userID": target, "docID": doc_ID, "docName": doc_name})
+
+}
+
+function save_doc() {
+    var doc = document.getElementById("editor").value;
+    server_socket.emit("save", {"docID": doc_ID, "doc": doc});
 }
